2e5866ec14f9ef8dedb8f49bc178372e5152f200
Federico Di Maio <f.dimaio@cineca.it>
2023-11-14 14:08:36 +0100

    refactoring

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ online train ticket purchase and sale service, preparatory project to access my 
 
 BUG:
 
-1- non trova Torre Spavento dopo averlo selezionato in tendina
+1- (landing.php)non trova Torre Spavento dopo averlo selezionato in tendina
 2- non salva gli id giusti all'interno della tabella carrozza_treno
 3- cercare treni per data
--- train_station/landing.php
+++ train_station/landing.php
@@ -13,120 +13,95 @@
 
     <?php
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "train_station";
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "train_station";
 
-    try {
-        $db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        print "ERRORE!: " . $e->getMessage() . "<br>";
-        die();
-    }
-
-    
-
-        
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $stazione_partenza = isset($_POST['partenza']) ? intval($_POST['partenza']) : null;
-    
-        if (empty($stazione_partenza)) {
-            echo 'Seleziona una stazione';
-            exit;
-        }
-        // Query per ottenere tutti i dati della stazione selezionata
-
-        $sql_stazione_partenza = "SELECT * FROM stazione WHERE id_stazione = :id";
-        $stmt_stazione_partenza = $db->prepare($sql_stazione_partenza);
-        $stmt_stazione_partenza->bindValue(':id', $stazione_partenza, PDO::PARAM_INT);
-        $stmt_stazione_partenza->execute();
-
-        $dati_stazione_partenza = $stmt_stazione_partenza->fetch(PDO::FETCH_ASSOC);
-
-        
-    
-    }
-
-
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $stazione_destinazione = isset($_POST['destinazione']) ? intval($_POST['destinazione']) : null;
-
-        if (empty($stazione_destinazione)) {
-            echo 'Seleziona una stazione';
-            exit;
+        try {
+            $db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            print "ERRORE!: " . $e->getMessage() . "<br>";
+            die();
         }
 
-        // Query per ottenere la posizione_km della stazione selezionata
-        $sql_posizione_km_destinazione = "SELECT posizone_km FROM stazione WHERE id_stazione = :id";
-        $stmt_posizione_km_destinazione = $db->prepare($sql_posizione_km_destinazione);
-        $stmt_posizione_km_destinazione->bindValue(':id', $stazione_destinazione, PDO::PARAM_INT);
-        $stmt_posizione_km_destinazione->execute();
-
-        $dati_posizione_km_destinazione = $stmt_posizione_km_destinazione->fetch(PDO::FETCH_ASSOC);
-
-        // Calcola la somma dei campi posizione_km delle due stazioni selezionate
-        $somma_posizione_km_destinazione = floatval($dati_posizione_km_destinazione['posizone_km']);
-
-
-        // Query per ottenere tutti i dati della stazione selezionata
-        $sql_stazione_destinazione = "SELECT * FROM stazione WHERE id_stazione = :id";
-        $stmt_stazione_destinazion = $db->prepare($sql_stazione_destinazione);
-        $stmt_stazione_destinazion->bindValue(':id', $stazione_destinazione, PDO::PARAM_INT);
-        $stmt_stazione_destinazion->execute();
-
-        $dati_stazione_destinazione = $stmt_stazione_destinazion->fetch(PDO::FETCH_ASSOC);
-
-
+        session_start();
         
 
-        // Moltiplica la somma dei km per 0,25 cent di euro
-        $costo_km = 0.25;
-        $costo_biglietto = $somma_posizione_km_destinazione * $costo_km;
-
+            
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $stazione_partenza = isset($_POST['partenza']) ? intval($_POST['partenza']) : null;
         
-    }
+            if (empty($stazione_partenza)) {
+                echo 'Seleziona una stazione di partenza';
+                exit;
+            }
+            
 
-    $dataPartenza = null;
-    $dataRitorno = null;
-    $orarioPartenza = null;
-    $orarioArrivo = null;
+            $sql_stazione_partenza = "SELECT * FROM stazione WHERE id_stazione = :id";
+            $stmt_stazione_partenza = $db->prepare($sql_stazione_partenza);
+            $stmt_stazione_partenza->bindValue(':id', $stazione_partenza, PDO::PARAM_INT);
+            $stmt_stazione_partenza->execute();
+
+            $dati_stazione_partenza = $stmt_stazione_partenza->fetch(PDO::FETCH_ASSOC);
+
+            
+        
+        }
 
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (isset($_POST["data-partenza"])) {
-            $dataPartenza = new DateTime($_POST["data-partenza"]);
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $stazione_destinazione = isset($_POST['destinazione']) ? intval($_POST['destinazione']) : null;
+
+            if (empty($stazione_destinazione)) {
+                echo 'Seleziona una stazione di destinazione';
+                exit;
+            }
+
+            // Query per ottenere la posizione_km della stazione selezionata
+            $sql_posizione_km_destinazione = "SELECT posizone_km FROM stazione WHERE id_stazione = :id";
+            $stmt_posizione_km_destinazione = $db->prepare($sql_posizione_km_destinazione);
+            $stmt_posizione_km_destinazione->bindValue(':id', $stazione_destinazione, PDO::PARAM_INT);
+            $stmt_posizione_km_destinazione->execute();
+
+            $dati_posizione_km_destinazione = $stmt_posizione_km_destinazione->fetch(PDO::FETCH_ASSOC);
+
+            // Calcola la somma dei campi posizione_km delle due stazioni selezionate
+            $somma_posizione_km_destinazione = floatval($dati_posizione_km_destinazione['posizone_km']);
+
+
+            // Query per ottenere tutti i dati della stazione selezionata
+            $sql_stazione_destinazione = "SELECT * FROM stazione WHERE id_stazione = :id";
+            $stmt_stazione_destinazion = $db->prepare($sql_stazione_destinazione);
+            $stmt_stazione_destinazion->bindValue(':id', $stazione_destinazione, PDO::PARAM_INT);
+            $stmt_stazione_destinazion->execute();
+
+            $dati_stazione_destinazione = $stmt_stazione_destinazion->fetch(PDO::FETCH_ASSOC);
+
+            // Moltiplico la somma dei km per 0,25 cent di euro
+            $costo_km = 0.25;
+            $costo_biglietto = $somma_posizione_km_destinazione * $costo_km;
+
             
         }
-    }
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (isset($_POST["data-ritorno"])) {
-            $dataRitorno = new DateTime($_POST["data-ritorno"]);
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (isset($_POST["data-partenza"])) {
+                $dataPartenza = new DateTime($_POST["data-partenza"]);
+            }
         }
-    }
 
-    if($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (isset($_POST["orario-partenza"])) {
-            $orarioPartenza = new DateTime($_POST["orario-partenza"]);
+        
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (isset($_POST["orario-partenza"])) {
+                $orarioPartenza = new DateTime($_POST["orario-partenza"]);
+            }
         }
-    }
 
-    if($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (isset($_POST["orario-arrivo"])) {
-            $orarioArrivo = new DateTime($_POST["orario-arrivo"]);
-        }
-    }
-    
-    //echo 'Stazione di partenza: ' . htmlspecialchars($dati_stazione_partenza['nome_stazione']) . '<br>';
-    //echo 'Stazione di destinazione: ' . htmlspecialchars($dati_stazione_destinazione['nome_stazione']) . '<br>';
-    //echo 'Costo del biglietto: ' . $costo_biglietto . ' euro' . '<br>';
-    //echo 'Data di partenza: ' . ($dataPartenza ? htmlspecialchars($dataPartenza->format('d-m-Y')) : 'Non ancora selezionata') . '<br>';
-    //echo 'Data di ritorno: ' . ($dataRitorno ? htmlspecialchars($dataRitorno->format('d-m-Y')) : 'Non ancora selezionata') . '<br>';
-    //echo 'Orario di partenza: ' . ($orarioPartenza ? htmlspecialchars($orarioPartenza->format('H:i')) : 'Non ancora selezionato') . '<br>';
-    //echo 'Orario di arrivo: ' . ($orarioArrivo ? htmlspecialchars($orarioArrivo->format('H:i')) : 'Non ancora selezionato') . '<br>';
-    
+        
+        
+   
     
     ?>
 
@@ -143,14 +118,14 @@
 
                 <?php
 
-                $sql = "SELECT * FROM stazione";
-                $result = $db->query($sql);
+                    $sql = "SELECT * FROM stazione";
+                    $result = $db->query($sql);
 
-                if ($result->rowCount() > 0) {
-                    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                        echo '<option value="' . intval($row["id_stazione"]) . '">' . htmlspecialchars($row["nome_stazione"]) . '</option>';
+                    if ($result->rowCount() > 0) {
+                        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                            echo '<option value="' . intval($row["id_stazione"]) . '">' . htmlspecialchars($row["nome_stazione"]) . '</option>';
+                        }
                     }
-                }
 
                 ?>
 
@@ -165,13 +140,13 @@
 
                 <?php
 
-                $result = $db->query($sql);
+                    $result = $db->query($sql);
 
-                if ($result->rowCount() > 0) {
-                    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                        echo '<option value="' . intval($row["id_stazione"]) . '">' . htmlspecialchars($row["nome_stazione"]) . '</option>';
+                    if ($result->rowCount() > 0) {
+                        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                            echo '<option value="' . intval($row["id_stazione"]) . '">' . htmlspecialchars($row["nome_stazione"]) . '</option>';
+                        }
                     }
-                }
 
                 ?>
 
@@ -192,9 +167,6 @@
             <input type="time" id="orario-partenza" name="orario-partenza" required>
         </div>
 
-
-
-
         <button type="submit">Cerca treni</button>
 
         
@@ -204,11 +176,10 @@
 
     <form action="./loginNonEffettuato.html" method="POST">
 
-        
-
         <button type="submit">prenota treno</button>
 
     </form>
+    
 
         <nav>
     
